397fdd22e38e3822bbf572af37ac7ebe11c90623
github username <github email address>
2018-04-24 16:30:07 -0400

    solved the labs

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,14 +1,26 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'awesome_print'
 
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  character_hash["results"].each do |character_info|
+      if character_info["name"].downcase == character
+        # character_info.each do |attribute, data|
+        #   binding.pry
+        # end
+        return character_info["films"].collect do |film_url|
+          film_info = RestClient.get(film_url)
+          film_hash = JSON.parse(film_info)
+        end
+      end
+    end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -18,9 +30,26 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
+# ap get_character_movies_from_api("C-3PO")
+
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  film_titles = []
+    films_hash.each do |film_details|
+      film_titles << film_details["title"]
+    end
+
+    # ap film_titles
+    film_titles.each.with_index do |title, index|
+      #binding.pry
+      puts "#{index+1} #{title}"
+    end
 end
+
+# parse_character_movies(get_character_movies_from_api("C-3PO"))
+
+
+
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
